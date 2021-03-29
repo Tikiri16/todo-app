f6349dacf6ae578a580344e319c8e2cc8d898fa5
tikiri16 <tikiri1995@gmail.com>
2021-03-29 18:06:58 +0530

    add submit todo feature

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+  addItem = async(e) => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.title !== '') {
+      const response = await fetch('https://app.serverless.com/tikiridiasena/apps/todo-app/todo/dev/us-east-1', {
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+          'User-Agent': 'todo',
+        },
+        body: JSON.stringify({title: newItem.title}),
+        method:'POST'
+      });
+      const json = await response.json();
+      const items = this.state.items;
+      items.unshift(json);
+      this.setState({ items: items });
+      this.inputElement.value = '';
+    }
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+          placeholder="Submit todo"
+          ref={c => {
+            this.inputElement = c;
+          }}
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}
+      />
+      <button onClick={this.addItem}>Add Task</button>
     </div>
   );
 }
