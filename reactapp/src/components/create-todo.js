a1bff8e224bb44756fa09fef2657d6387845704e
tikiri16 <tikiri1995@gmail.com>
2021-03-31 19:11:02 +0530

    Link create-todo to AWS DynamoDB

--- reactapp/src/components/create-todo.js
+++ reactapp/src/components/create-todo.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class TodosList extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            task: '',
             error: null
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     renderError() {
@@ -17,20 +21,33 @@ export default class TodosList extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleCreate.bind(this)}>
-                <input type="text" placeholder="What do I need to do?" ref="createInput" />
-                <button>Create</button>
-                {this.renderError()}
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Todo:</label>
+                    <input
+                        type="text"
+                        onChange={this.handleChange}
+                        placeholder="submit a task"
+                        value={this.state.name}
+                    />
+
+                    <button type="submit">Submit</button>
+                    {this.renderError()}
+                </form>
+            </div>
         );
     }
 
-    handleCreate(event) {
+    handleChange(event) {
         event.preventDefault();
 
-        const createInput = this.refs.createInput;
-        const task = createInput.value;
-        const validateInput = this.validateInput(task);
+        const inputValue = event.target.value;
+        const task = event.target.task;
+        const validateInput = this.validateInput(inputValue);
+        this.setState({
+            [task]: inputValue,
+        });
+        console.log(this.state);
 
         if (validateInput) {
             this.setState({ error: validateInput });
@@ -39,13 +56,22 @@ export default class TodosList extends React.Component {
 
         this.setState({ error: null });
         this.props.createTask(task);
-        this.refs.createInput.value = '';
+        this.event.target.value = '';
     }
 
-    validateInput(task) {
-        if (!task) {
+    async handleSubmit(event) {
+        event.preventDefault();
+        const task = this.state;
+        await axios.post(
+            'https://kdp2eooeia.execute-api.us-east-2.amazonaws.com/default/serverlessAppFunction',
+            { key1: `${task}` }
+        );
+    }
+
+    validateInput(inputValue) {
+        if (!inputValue) {
             return 'Please enter a task.';
-        } else if (_.find(this.props.todos, todo => todo.task === task)) {
+        } else if (_.find(this.props.todos, todo => todo.task === inputValue)) {
             return 'Task already exists.';
         } else {
             return null;
